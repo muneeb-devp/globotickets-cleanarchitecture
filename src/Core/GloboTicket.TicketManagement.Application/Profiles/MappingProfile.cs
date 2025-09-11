c02388cde2a08027cc065786cff1da7f59d65a5c
Muneeb Mughal <muneeb.devp@gmail.com>
2025-09-12 04:16:20 +0500

    Mapping profile: Add AutoMapper profile for entities and DTOs

---
+++ src/Core/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,26 @@
+using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvent;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
+using GloboTicket.TicketManagement.Application.ViewModels;
+
+namespace GloboTicket.TicketManagement.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+  public MappingProfile()
+  {
+    CreateMap<Domain.Entities.Event, EventListVm>().ReverseMap();
+    CreateMap<Domain.Entities.Event, EventDetailVm>().ReverseMap();
+    CreateMap<Domain.Entities.Event, CreateEventCommand>().ReverseMap();
+    CreateMap<Domain.Entities.Event, UpdateEventCommand>().ReverseMap();
+    CreateMap<Domain.Entities.Event, DeleteEventCommand>().ReverseMap();
+
+    CreateMap<Domain.Entities.Category, CategoryDto>().ReverseMap();
+    CreateMap<Domain.Entities.Category, CategoryEventListVm>().ReverseMap();
+    CreateMap<Domain.Entities.Category, CategoryListVm>().ReverseMap();
+  }
+}
