7313119fe272f38a4ae01fc730dba5b063977e53
Muneeb Mughal <muneeb.devp@gmail.com>
2025-09-12 04:15:44 +0500

    Repository and project file: Add repository interfaces and update project references

--- src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -4,5 +4,5 @@ namespace GloboTicket.TicketManagement.Application.Contracts.Persistence;
 
 public interface ICategoryRepository : IRepository<Category>
 {
-
+  Task<List<Category>> GetCategoriesWithEvents(bool includeHistory = false);
 }
--- src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -4,5 +4,5 @@ namespace GloboTicket.TicketManagement.Application.Contracts.Persistence;
 
 public interface IEventRepository : IRepository<Event>
 {
-
+  public Task<bool> IsEventNameAndDateUnique(string name, DateTime date, CancellationToken cancellationToken);
 }
