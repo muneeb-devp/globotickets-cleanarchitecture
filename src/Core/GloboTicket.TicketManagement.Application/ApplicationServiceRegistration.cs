a07d483d4d047d656f8d690332daf5303fe7217e
Muneeb Mughal <muneeb.devp@gmail.com>
2025-09-12 04:15:47 +0500

    Service registration and exception: Add ApplicationServiceRegistration and BadRequestException

---
+++ src/Core/GloboTicket.TicketManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+using System.Net.NetworkInformation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTicket.TicketManagement.Application;
+
+public static class ApplicationServiceRegistration
+{
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+  {
+
+    services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    return services;
+  }
+}
---
+++ src/Core/GloboTicket.TicketManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GloboTicket.TicketManagement.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+  public BadRequestException() : base() { }
+  public BadRequestException(string message) : base(message) { }
+  public BadRequestException(string message, Exception innerException) : base(message, innerException) { }
+}
